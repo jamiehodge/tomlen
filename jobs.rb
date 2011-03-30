34a3a6c0be37d90e8b30fc778960fd6d0a15b7bc
Jamie Hodge <jamiehodge@me.com>
2011-03-30 11:23:31 +0200

    give beanstalk time to spin up

--- jobs.rb
+++ jobs.rb
@@ -1,6 +1,14 @@
 $:.unshift File.expand_path('lib')
 require 'movie'
 
+timeout = 0
+while (timeout < 5)
+  break if `ps aux` =~ /beanstalkd$/
+  sleep 3
+  timeout += 1
+end
+exit(1) if timeout == 5
+
 include Stalker
 
 job 'movie.create' do |params|
